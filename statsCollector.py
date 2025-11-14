ce1faf28527a9d5bd8c12b8328f748f7422e61cd
JohanVerne <johan.verne2003@gmail.com>
2025-11-14 11:34:50 +0100

    Modified env variable collection

--- statsCollector.py
+++ statsCollector.py
@@ -5,16 +5,16 @@ import os
 
 
 def setup_spotify_client():
-    CLIENT_ID = os.environ.get("CLIENT_ID")
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-    REDIRECT_URI = os.environ.get("CLIENT_REDIRECT_URI")
+    SPOTIPY_CLIENT_ID = os.environ.get("SPOTIPY_CLIENT_ID")
+    SPOTIPY_CLIENT_SECRET = os.environ.get("SPOTIPY_CLIENT_SECRET")
+    SPOTIPY_REDIRECT_URI = os.environ.get("SPOTIPY_REDIRECT_URI")
     SCOPE = "user-library-read user-top-read user-read-recently-played user-read-playback-state"
 
     sp = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET,
-            redirect_uri=REDIRECT_URI,
+            client_id=SPOTIPY_CLIENT_ID,
+            client_secret=SPOTIPY_CLIENT_SECRET,
+            redirect_uri=SPOTIPY_REDIRECT_URI,
             scope=SCOPE,
         )
     )
